475dd548071226859843fba8f12cb2a2cd4b76f2
Agnieszka Maule <anna.maulle@gmail.com>
2017-04-14 10:16:42 -0300

    Perfect Square problem resolved

---
+++ perfectSquare.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int perfectSquare(int x){
+  int aux;
+  float n, y,y2, aux2;
+  n=1;
+  while(n<=x){
+    y = x/n;
+    printf("y/n = %f \n", y);
+    y = sqrt(y);
+    printf("sqrt(y)= %f\n", y);
+    y2 =y;
+    aux = (int) y;
+    printf("y: %f\n", y);
+    printf("aux=%d\n",aux);
+
+    aux2 = (float) aux;
+    printf("%f - %f\n", y2, aux2);
+    if((y2-aux2) == 0){
+      return n;
+    }
+    n++;
+  }
+}
+
+int main(){
+  int n, x;
+  printf("Give a value to check:\n" );
+  scanf("%d", &x);
+
+  n = perfectSquare(x);
+
+  printf("%d must be divided by %d to be a perfect square!", x, n);
+  return 0;
+}
