b7af560cf9ebce8767e4c3a9680fa517427ffc47
Agnieszka Maule <anna.maulle@gmail.com>
2017-04-18 10:53:39 -0300

    adding functiong popAt()

--- setOfStacks.java
+++ setOfStacks.java
@@ -10,7 +10,9 @@ public class Stack<T>{
     }
   }
 
-  private stackNode<T> top;
+  public stackNode<T> top;
+  // 3.3
+  public stackNode<T> bottom;
   //(3.2)getting minimum value at O(1)
   private T min;
   private int size;
@@ -29,6 +31,7 @@ public class Stack<T>{
     if(min==NULL){
       min = value;
       size = 1;
+      bottom = newNode; // 3.3
     }else{
       if(value < min){
         newNode.prevMin = min;
@@ -36,7 +39,6 @@ public class Stack<T>{
         size++;
       }
     }
-
     newNode.data = value;
     newNode.next =  top;
     top = newNode;
@@ -71,7 +73,7 @@ public class setOfStacks<int>{
 
   public void push(int v){
     private Stack last = getLastStack(); //who implements getLastStack??
-    if(last.siz == threshold){
+    if(last.size == threshold){
        Stack newStack = new Stack(); // or pass the capacity/threshold as a parameter
        newStaack.push(v);
        setStack.add(newStack);
@@ -79,4 +81,35 @@ public class setOfStacks<int>{
       last.push(v);
     }
   }
+
+  public int pop(){
+    private Stack last = getLastStack();
+    if(last.top == null) throw new EmptyQueueExeption();
+    return last.pop;
+  }
+
+  public Stack getLastStack(){
+    int index = setStack.size() - 1;
+    return setStacks[index];
+  }
+  // assuming the user will assume the vectors position from 0 to n;
+  public void popAt(int index){
+    int value;
+    if(setStack[index]==null) trow new EmptyQueueExeption();
+    if(setStack[index+1]==null){
+        return popStack.pop();
+    }else{
+        value = setStack[index].pop();
+        while(setStack[index+1]!=null){
+            setStack[index].top.next = setStack[index+1].bottom;
+            setStack[index].top = setStack[index+1].bottom;
+            if(setStack[index+1].size > 0){
+              setStack[index+1].bottom = setStack[index+1].bottom.next;
+            }
+            index = index+1;
+        }
+    }
+    return value;
+  }
+
 }
